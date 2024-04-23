3b00e3cafb5e3d6b37398496034296e2e1c12083
Jonathan Hachette <jonathan.hachette@gmail.com>
2024-04-23 11:53:11 +0200

    ajout dossier assets

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,15 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+
+
 function Banner() {
-    return <h1>The Jungle House</h1>
+    const title = 'The Jungle House'
+    return (
+    <div>
+        <img src={logo} alt="logo" className='jh-logo' />
+        <h1 className="jh-banner">{title}</h1>
+    </div>     
+    )
 }
 
 export default Banner
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,20 @@
-function Cart () {
-    const CartContent = ["Monstera ", "Lierre ", "Bouquet de fleurs"]
-    const PrixMonstera = 8
-    const PrixLierre = 10
-    const PrixBouquet = 15
-    return ( <div> 
-            <h2>Panier</h2>
-            <ul> 
-                <li> {CartContent[0]}  {PrixMonstera}€</li>
-                <li> {CartContent[1]} {PrixLierre} €</li>
-                <li> {CartContent[2]} {PrixBouquet} € </li>
-            </ul>
-                Votre total est de {PrixMonstera + PrixLierre + PrixBouquet} €
-            </div>    
-        )
+import '/Users/JOHN/Documents/Code/Cours/OpenClassrooms/REACT/jungle-house/src/styles/Cart.css'
+
+function Cart() {
+	const monsteraPrice = 8
+	const ivyPrice = 10
+	const flowerPrice = 15
+	return (
+		<div className='jh-cart'>
+			<h2>Panier</h2>
+			<ul>
+				<li>Monstera : {monsteraPrice}€</li>
+				<li>Lierre : {ivyPrice}€</li>
+				<li>Fleurs : {flowerPrice}€</li>
+			</ul>
+			Total : {monsteraPrice + ivyPrice + flowerPrice}€
+		</div>
+	)
 }
 
 export default Cart
